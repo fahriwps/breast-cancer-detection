97228f207c3f5687134d83acfe58ece85af93e23
fahriwps <fahriprs@ctemplar.com>
2020-08-28 22:10:14 +0700

    add and fix run.py

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv) 
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = gui_window.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
